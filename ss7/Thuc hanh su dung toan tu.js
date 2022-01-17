8d95f3302aeb02227982eb7afc4bbc38488ca5bb
Chuducanh <chanhducu@gmail.com>
2022-01-17 19:27:11 +0700

    ss7

---
+++ ss7/Thuc hanh su dung toan tu.js
@@ -0,0 +1,7 @@
+function dienTich()
+{
+    let width = prompt('Nhập vào chiều rộng HCN:(cm) ');
+    let height = prompt("Nhập vào chiều dài HCN:(cm) ");
+    let tich = width * height;
+    document.write("Diện tích HCN là: " + tich +' cm^2');
+}
